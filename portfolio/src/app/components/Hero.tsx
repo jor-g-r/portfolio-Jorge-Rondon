d71e462e3197096eae748afe5df9b186c16b44fc
jor-g-r <jfrrj19@gmail.com>
2023-08-19 20:22:53 -0500

    hero responsive adjustments, navbar responsive

--- portfolio/src/app/components/Hero.tsx
+++ portfolio/src/app/components/Hero.tsx
@@ -15,7 +15,7 @@ export default function Hero () {
     const { scrollYProgress } = useScroll();
     return(
         <>
-         <section className="h-[89vh] hero-bg border-b border-solid border-dark-500" id='hero'>
+         <section className="h-[89vh] hero-bg border-b border-solid border-dark-500 overflow-x-hidden" id='hero'>
 
             <div className="container mx-auto lg:px-16 h-full">
                 
@@ -23,15 +23,17 @@ export default function Hero () {
                 <div className="relative flex flex-col justify-center  h-full">
 
                     <div className="text-center condensed  pb-56">
-                        <motion.h1 className="text-8xl font-bold"
+                        <motion.h1 className="lg:text-8xl text-7xl font-bold"
                             initial={{ y: 0,  }} 
                             animate={{ y: [0, 5, 0], rotate: [0, -1, 0] }}
                             transition={{ duration: 8, repeat: Infinity, repeatType: 'reverse' }} >
                             Frontend Developer
                         </motion.h1>
                         <h3 className='text-3xl text-primary pt-2 pb-4'>
-                            Jorge Rondón
+                            Jorge Francisco
                         </h3>
+
+
                         <button className="ui-btn mx-auto">
                             <span className='flex items-center gap-1 condensed font-light uppercase'>
                                 Resume
@@ -41,17 +43,17 @@ export default function Hero () {
                        
                     </div>
 
-                    <div className="flex align w-2/3 absolute bottom-14 gap-8"
+                    <div className="flex align ml-2 px-2 w-full lg:w-2/3 absolute bottom-14 gap-4 lg:gap-8"
                          onMouseEnter={() => setIsHovered(true)}
                          onMouseLeave={() => setIsHovered(false)}
                     >
                         <div className="self-end pb-3">
                             <IconArrowDown isHovered={isHovered}/>
                         </div>
-                        <div className="text-xl tracking-tight leading-10 cursor-default">
-                            <p>Experienced Web Developer with strong UI/UX skills.</p>
-                            <p>Skilled in CMS and WordPress environments.</p>
-                            <p>Passionate about new technologies and a fan of hands-on learning.</p>
+                        <div className="lg:text-xl tracking-tight leading-8 lg:leading-10 cursor-default">
+                            <p className='drop-shadow-lg'>Experienced Web Developer with strong UI/UX skills.</p>
+                            <p className='drop-shadow-lg'>Skilled in CMS and WordPress environments.</p>
+                            <p className='drop-shadow-lg'>Passionate about new technologies and a fan of hands-on learning.</p>
                         </div>
                     </div>   
                         
@@ -62,17 +64,18 @@ export default function Hero () {
             </div>
     
         </section>
-        <div  className="w-full min-h-[44px] border-b border-solid border-dark-500 overflow-hidden flex items-center">  
+        <div  className="w-full min-h-[44px] border-b border-solid border-dark-500 overflow-y-hidden flex items-center">  
 
           
                 
             <motion.h4 
-            className='m-auto text-dark-400 font-light text-xl tracking-[14px] uppercase'
+            className='m-auto flex flex-col sm:flex-row justify-center text-center leading-6 py-2
+                     text-white-500 font-light lg:text-xl tracking-[10px] lg:tracking-[14px] uppercase'
             style={{
                 opacity: scrollYProgress,
                 transition: 'opacity 0.125s ease-in-out',
               }}>
-                   Web Design & Web Development
+                 <span>Web Design &</span> &nbsp;Web Development
             </motion.h4>
          
         </div>
--- portfolio/src/app/components/Navbar.tsx
+++ portfolio/src/app/components/Navbar.tsx
@@ -1,4 +1,8 @@
+"use client";
+import {useState} from 'react';
 import Link from 'next/link';
+import {motion} from 'framer-motion';
+
 
 interface NavbarProps {
   title: string;
@@ -14,9 +18,9 @@ interface NavbarBtnProps {
 
 const NavbarBtn: React.FC<NavbarBtnProps> = ({href, title}) => {
   return (
-    <Link href={href} className='group relative'>
+    <Link href={href} className='group relative tracking-widest'>
         {title}
-      <span className='h-[1px] w-0  bg-white-100 absolute left-0 -bottom-0.5
+      <span className='h-[1px] w-0 bg-white-100 absolute left-0 -bottom-0.5
       group-hover:w-full transition-all ease-in-out duration-500'>
         &nbsp;
       </span>
@@ -24,34 +28,92 @@ const NavbarBtn: React.FC<NavbarBtnProps> = ({href, title}) => {
   )
 }
 
+
+
 const Navbar: React.FC<NavbarProps> = (props) => {
-  
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const [isNavVisible, setIsNavVisible] = useState(false);
+
+  const toggleNav = () => {
+    setIsNavVisible((prevState) => !prevState);
+  };
 
   return (
     <>
       <nav className={`w-full md:px-7 z-40 bg-dark-900 shadow-nav-shadow`}>
         <div className="container mx-auto">
-          <div className="flex justify-between lg:px-16  py-3">
+
+          
+          <motion.div className=" flex flex-col lg:flex-row justify-between lg:items-center px-10 lg:px-16  py-3  transition-transform duration-500 ease-in "
+          
+          
+          >  {/* Este contenedor */}
+
+              
+            <div className="flex justify-between">
+              <div className="cursor-default">
+                <h5 className={`group lg:text-sm text-lg font-bold ease-in duration-500 text-dark-900 bg-white-100 
+                              hover:bg-yellow-400 h-14 w-14  lg:h-10 lg:w-10 pr-[6px] p-1 flex items-end justify-end rounded tracking-tighter`}>
+                  J 
+                  <span className='group-hover:hidden group-hover:opacity-0 ease-in transition-opacity duration-500'>F</span>
+                  <span className='hidden group-hover:block opacity-0 group-hover:opacity-100 ease-in duration-500'>S</span>
+                </h5>
+              </div>
 
 
-            <div className="">
-              <h5 className={`condensed text-2xl`}>
-                JFR DEV
-              </h5>
+              <div className="lg:hidden">
+                  <div className= {`w-24 items-center justify-center ${menuOpen ? 'active' : ''} `}
+                                  onClick={toggleNav}>
+
+                    <div className={`group flex h-14 w-14 cursor-pointer float-right items-center justify-center rounded border 
+                                   border-dark-600 bg-white p-2  hover:bg-dark-900 ${ menuOpen ? 'border-dark-400' : ''}`} 
+                                    onClick={() => setMenuOpen(!menuOpen)} >
+                      <div className="space-y-2">
+                        <span className={`block h-0.5 w-9 origin-center bg-white-300 transition-transform duration-500 ease-in-out 
+                                          ${ menuOpen ? 'translate-y-[5px] rotate-45' : ' ' }
+                                          `}>
+                        </span>
+                        <span className={`block h-0.5  origin-center bg-orange-500 transition-transform duration-500 ease-in-out 
+                                          ${ menuOpen ? 'w-9 -translate-y-[5.1px] -rotate-45' : 'w-7' }
+                                          `}>
+                        </span>
+                      </div>
+                    </div>
+
+                  </div>
+              </div>
+
             </div>
             
             <div className="lg:w-2/3 w-4/5 max-w-xl">
-              <ul className='flex justify-between gap-3 pt-1'>
+              <motion.ul className={`lg:flex  flex-col lg:flex-row 
+                              pb-3 lg:pb-0 justify-between lg:gap-3 gap-y-2 pt-6 lg:pt-1 uppercase duration-300 ease-in transition-transform
+                              ${ isNavVisible ? 'pt-6 flex   ' : 'pt-0 hidden' }`}
+                                
+                                initial={{ opacity: 0, maxHeight: 1 }} // Estado inicial (oculto)
+                                animate={{
+                                  opacity: isNavVisible ? 1 : 0,
+                                  maxHeight: isNavVisible ? 600 : 0, // Altura máxima cuando es visible
+                                }}
+                                transition={{ duration: .3, ease: "easeIn" }} // Configuración de la transición
+                               >
+
                 <li><NavbarBtn title='About Me' href="#about"  /></li>
                 <li><NavbarBtn title='Projects' href="#projects"  /></li>
                 <li><NavbarBtn title='Backgrounds' href="#backgrounds " /></li>
                 <li><NavbarBtn title='Experience' href="#experience"  /></li>
-                <li><NavbarBtn className='bg-white-100 text-dark-600 px-4 py-1 rounded-sm' title='Contact' href="#" /></li>
-              </ul>
+                <li className='mt-2 lg:mt-0'>
+                  <Link className='bg-white-100 text-dark-600 px-4  py-1 rounded-sm hover:bg-primary duration-300 ease-in' href="#">Contact</Link>
+                </li>
+              </motion.ul>
             </div>
 
 
-          </div>
+          
+
+
+          </motion.div>
         </div>
       </nav>
     </>
