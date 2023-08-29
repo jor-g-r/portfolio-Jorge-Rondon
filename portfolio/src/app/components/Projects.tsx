e4a58ce3936ff60078c7f5af2299743d54f37985
jor-g-r <jfrrj19@gmail.com>
2023-08-29 15:33:59 -0500

    Project section - object structure

---
+++ portfolio/src/app/components/Projects.tsx
@@ -0,0 +1,15 @@
+import imgProject from '../img/prev-project1.jpg'
+
+
+const projectsInitialData = [
+    {
+        name: ' name',
+        thumbnail: imgProject,
+        url: 'url',
+        description: ' Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit est molestias temporibus, provident ducimus cupiditate similique excepturi architecto voluptate perferendis. Culpa ab nemo reprehenderit, expedita quae dignissimos aliquid fugit dolorum. ',
+        tags: ['Javascript', 'Tailwind.css', 'React.js'],
+        row: 2,
+        col: 1,
+    }
+
+]
