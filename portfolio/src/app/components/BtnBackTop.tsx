ef43e0bfe289abf2bf285659ea9cdd62a173fc3e
Jorge Rondón <jfrrj19@gmail.com>
2023-12-18 15:42:07 -0500

    back to top

--- portfolio/src/app/components/BtnBackTop.tsx
+++ portfolio/src/app/components/BtnBackTop.tsx
@@ -23,7 +23,7 @@ export default function BtnBackTop (){
             <span>to</span>
             <span>top</span>
         </div>
-        <svg width="20px" xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" 
+        <svg width="20px" xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 backtop" fill="none" 
              viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
 
             <path strokeLinecap="round" strokeLinejoin="round" d="M14 5l7 7m0 0l-7 7m7-7H3"></path>
